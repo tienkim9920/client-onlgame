fe40450150f594cb70fb29254d718b71c2bcaafa
tienkim9920 <tienkim9920@gmail.com>
2021-08-19 21:11:33 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+// import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Caro from './Component/Caro';
 
 function App() {

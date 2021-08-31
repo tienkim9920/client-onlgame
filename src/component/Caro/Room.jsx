c38406069fd253f7549b9fd1f282563a2d99ba1a
tienkim9920 <tienkim9920@gmail.com>
2021-08-31 22:34:40 +0700

    update

--- src/component/Caro/Room.jsx
+++ src/component/Caro/Room.jsx
@@ -85,7 +85,7 @@ function Room(props) {
     const createRoom = () => {
         console.log("Tao Phong")
 
-        const roomID = Math.floor(Math.random() * 999)
+        const roomID = Math.floor(Math.random() * 999).toString()
 
         socket.emit('room', roomID)
 
